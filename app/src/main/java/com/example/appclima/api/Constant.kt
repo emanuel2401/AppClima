9165cd0da495f4444ba173bf1fc92310618b703d
emanuel gonzalez <emanuel.gonzalez@metafar.io>
2024-06-17 17:33:10 -0300

    initial

---
+++ app/src/main/java/com/example/appclima/api/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.appclima.api
+
+object Constant {
+
+    val apiKey = "a2d9ac5408e14276ac3170326241606"
+}
---
+++ app/src/main/java/com/example/appclima/model/WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.example.appclima.model
+
+data class WeatherModel(
+    val current: Current,
+    val location: Location
+)
